7cee4d0dd16117b219d7dc2cade1901de87daa78
Rebekah Aimee Yoder <rebekahaimeeyoder@gmail.com>
2016-04-11 11:24:54 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+
+#from kivy.uix.boxlayout import BoxLayout
+#from kivy.uix.anchorlayout import AnchorLayout
+#from kivy.uix.listview import ListView, ListItemButton
+#from kivy.factory import Factory
+#from kivy.properties import ListProperty, StringProperty, ObjectProperty
+# We'll probably need these later, when we want to modify their logic in the
+# Python file.
+
+class YpnApp(App):
+    pass
+
+
+
+if __name__ == '__main__':
+    YpnApp().run()
