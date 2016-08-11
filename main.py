bf6c27b8986bfde53ee2e25860387ab7221447bd
Rebekah Aimee Yoder <rebekahaimeeyoder@gmail.com>
2016-08-10 22:43:22 -0500

    Added functionality to menu's close and back buttons

--- main.py
+++ main.py
@@ -32,21 +32,32 @@ class YpnApp(App):
     pass
 
 class RootWidget(BoxLayout):
+    # variable to contain a three-char slug denoting the last non-menu page
+    # opened by user. Used by the Back button on the menu.
+    last_page_open = "cal"
+    # nts: this might be kind of weird with the discounts page if the menu
+    # button isn't disabled while coupons are open. make sure there's no way
+    # for users to "cheat" for multiple uses of coupons.
+
     def display_calendar_page(self):
         self.clear_widgets()
         self.add_widget(CalendarPage())
+        self.last_page_open = "cal"
 
     def display_discounts_page(self):
         self.clear_widgets()
         self.add_widget(DiscountsPage())
+        self.last_page_open = "dis"
         
     def display_articles_page(self):
         self.clear_widgets()
         self.add_widget(ArticlesPage())
+        self.last_page_open = "art"
         
     def display_about_us_page(self):
         self.clear_widgets()
         self.add_widget(AboutUsPage())
+        self.last_page_open = "abo"
     
     #def display_search_page(self):
     #    self.clear_widgets()
@@ -55,6 +66,20 @@ class RootWidget(BoxLayout):
     def display_menu_page(self):
         self.clear_widgets()
         self.add_widget(MenuPage())
+
+    def display_last_page(self):
+        # clumsy programming style. Python doesn't have switch statements; I
+        # will neaten up this code in the way you're supposed to later, when I
+        # don't have so many other things on my mind. Right now I just want it
+        # to work and if it doesn't, I want to know this isn't the problem.
+        if self.last_page_open == "cal":
+            self.display_calendar_page()
+        if self.last_page_open == "dis":
+            self.display_discounts_page()
+        if self.last_page_open == "art":
+            self.display_articles_page()
+        if self.last_page_open == "abo":
+            self.display_about_us_page()
         
 class CalendarPage(BoxLayout):
     pass
